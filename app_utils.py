661e26012d1c5c5f824bd01004271cd33d6f5b7e
fernandocfbf <fernandocfbf@al.insper.edu.br>
2021-05-17 23:21:16 -0300

    Consertei o arquivo, acomplando códigos em funções, reduzindo loops e organizando em arquivos separados

---
+++ app_utils.py
@@ -0,0 +1,130 @@
+from cmath import *
+from math import *
+from numpy import linalg
+import numpy as np
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.linalg
+
+def distancia_entre_pontos(x1, x2, y1, y2):
+    """
+    função que calcula a distância entre dois pontos
+    recebe: coordenadas de dois pontos [inteiro]
+    retorna: distância entre os pontos [inteiro]
+    """
+    
+    return sqrt((x2 - x1)**2 + (y2 - y1)**2)
+
+def matriz_conectividade(numero_do_membro, incidencia, numero_de_elementos=3):
+    """
+    função que calcula a matriz de conectividade de um elemento específico
+    recebe: número do elemento [inteiro] e a matriz de incidência lida do excel [matriz]
+    retorna: conectividade [lista]
+    """
+    
+    conectividade = numero_de_elementos*[0]
+    
+    # O numero do membro-1 é a linha a matriz que eu tenho que utilizar
+    no_1 = int(incidencia[numero_do_membro-1, 0])
+    no_2 = int(incidencia[numero_do_membro-1, 1])
+    
+    conectividade[no_1-1] = -1
+    conectividade[no_2-1] = 1
+
+    return conectividade
+
+def conec_global_T(nos, incidencia):
+    """
+    função responsável por devolver a matriz de conectividade global
+    recebe: número de nós [inteiro], matriz de incidencia [matriz]
+    retorna: matriz onde cada linha é uma conectividade de um membro [matriz]
+    """
+  
+    C = []
+    for i in range(nos):
+        C.append(matriz_conectividade(i+1, incidencia)) #repare que i começa em 0
+    return np.array(C).T
+
+
+def calculate_Se(n_membro, m_incidencia, m_nos, m_membros):
+    """ 
+    Função responsável por calcular o valor de Se para um dado elemento
+    recebe: número do membro [inteiro], matriz de incidência [matriz], matriz dos nós [matriz], matriz dos membros [matriz]
+    retorna: valor de Se para o elemento escolhido [matriz/inteiro]
+    """
+    
+    E = m_incidencia[n_membro-1, 2] #elemento linear elástico
+    A = m_incidencia[n_membro-1, 3] #área da senção transversal
+    
+    no_1 = int(m_incidencia[n_membro-1, 0])
+    no_2 = int(m_incidencia[n_membro-1, 1])
+    
+    # Pegar a coordenada do no_1
+    x_no1 = m_nos[0, no_1-1]
+    y_no1 = m_nos[1, no_1-1]
+    
+    # Pegar a coordenada do no_2
+    x_no2 = m_nos[0, no_2-1]
+    y_no2 = m_nos[1, no_2-1]
+    
+    l = distancia_entre_pontos(x_no2, x_no1, y_no2, y_no1)
+    
+    cordeenadas_membro = m_membros[:, n_membro-1] #pega as coordenadas do membro
+    coornadas_matriz = np.array([cordeenadas_membro]) #transforma em matriz
+    coornadas_matriz_T = coornadas_matriz.T #calcula a transposta
+    me = sum(i**2 for i in cordeenadas_membro)
+    segunda_parte = (np.dot(coornadas_matriz_T, coornadas_matriz))/me
+    return ((E*A)/l)*segunda_parte
+
+def  calculate_K(n_membro, m_incidencia, m_nos, m_membros):
+    """
+    função responsável por calcular a matriz K para um elemento
+    recebe: número do membro [inteiro], matriz incidência, matriz de nós e matriz de membros
+    retorna: matriz K para o dado elemento
+    """
+    
+    MC = np.array([matriz_conectividade(n_membro, m_incidencia)]) #matriz conectividade
+    MC_T = np.transpose(MC) #matriz conectividade transposta
+    Se = calculate_Se(n_membro, m_incidencia, m_nos, m_membros)
+    dot = MC * MC_T
+    return np.kron(dot, Se) 
+
+def matriz_global(elementos, m_incidencia, m_nos, m_membros):
+    """
+    função responsável por realizar a somatória das matrizes K de cada elementos
+    recebe: número de membros [inteiro], matriz incidência, matriz de nós e matriz de membros
+    retorna: somatória das matrizes K de cada elemento [matriz]
+    """
+    
+    kg = np.zeros((elementos*2, elementos*2)) #sempre vai ser num_membros*2
+    for i in range(elementos):
+        kg += calculate_K(i, m_incidencia, m_nos, m_membros)        
+    return kg
+
+def MR_para_solucao(matriz):
+    """
+    função responsável por excluir as colunas/linhas desejadas de uma matriz
+    recebe: matriz
+    retorna: matriz com as linhas e colunas desejadas apagadas
+    """
+    
+    matriz_certa = np.zeros((6, 6))
+    matriz_certa = np.delete(matriz, (0, 2, 3), axis=0) #apaga 1º, 3º e 4º linha
+    matriz_certa = np.delete(matriz_certa, (0, 2, 3), axis=1) #apaga 1º, 3º e 4º coluna
+    return matriz_certa
+
+def calcula_deslocamentos(matriz_rigidez, matriz_força):
+    L,U = scipy.linalg.lu(matriz_rigidez, permute_l=True)
+    y = scipy.linalg.solve(L, matriz_força)
+    x = scipy.linalg.solve_triangular(U, y)
+    return x
+
+def calculate_force(matriz_k, u, linha_number):
+    """
+    função responsável por calcular a força para um dado elemento
+    recebe: matriz K do elemento, matriz u (completo), número da linha do elemento
+    """
+    
+    linha = matriz_k[linha_number,:] #pega a linha desejada na matriz
+    return np.dot(linha, u) #retorna multiplicação de linha X coluna
